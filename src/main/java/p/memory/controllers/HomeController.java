5a98b1f362c07e6af7df09e441ac4153c824b39f
LAPTOP-IIK6U1LR\asdfg <asdfg0992@gmail.com>
2023-06-08 16:24:50 +0900

    gitignore 수정

--- src/main/java/p/memory/controllers/HomeController.java
+++ src/main/java/p/memory/controllers/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	@RequestMapping"/")
+	@RequestMapping("/")
 	public String home() {
 		
 		return "home";
